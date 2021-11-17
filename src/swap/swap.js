256135ee1cfc7a2174825d91ae738b21bb9e9942
Angela Louh <louh.angela@gmail.com>
2021-11-16 17:35:02 -0800

    Completed the swap nodes problem

--- src/swap/swap.js
+++ src/swap/swap.js
@@ -4,6 +4,33 @@
  * @param {Node} x
  * @param {Node} y
  */
-function swap(list, x, y) {}
+function swap(list, x, y) {
+    if (!list.length) {
+        return list;
+    }
+
+    let x_next = x.next;
+    let x_prev = list.findWithPrevious(node => node === x)[1];
+    let y_prev = list.findWithPrevious(node => node === y)[1];
+
+    x.next = y.next;
+    y.next = x_next;
+
+    // If x_prev exists, it means x is not the head of the list
+    if (x_prev) {
+        x_prev.next = y;
+    } else {
+        list.head = y;
+    }
+
+    // If y_prev exists, it means y is not the head of the list
+    if (y_prev) {
+        y_prev.next = x;
+    } else {
+        list.head = x;
+    }
+
+    return list;
+}
 
 module.exports = swap;
